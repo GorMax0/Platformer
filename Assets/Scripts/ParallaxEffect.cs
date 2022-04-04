6726e108d5a268d351b1d6a764bcf388e0c7f34d
GorMax0 <102469814+GorMax0@users.noreply.github.com>
2022-04-04 22:18:28 +0300

    Added parallax effect

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    [SerializeField] private Transform _followingTarget;
+    [SerializeField, Range(0f, 1f)] private float _parallaxStrength;
+
+    private Vector3 _targetPreviousPosition;
+
+    private void Start()
+    {
+        if (_followingTarget == null)
+        {
+            _followingTarget = Camera.main.transform;
+        }
+
+        _targetPreviousPosition = _followingTarget.position;
+    }
+
+    private void Update()
+    {
+        var delta = _followingTarget.position - _targetPreviousPosition;
+
+        _targetPreviousPosition = _followingTarget.position;
+        transform.position += delta * _parallaxStrength;
+    }
+}
